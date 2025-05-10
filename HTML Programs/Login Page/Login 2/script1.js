11ff4c4c159175146f047c290fd45534ead5bd29
HasanIqbal8090 <newcreations7890wait@gmail.com>
2025-05-10 20:45:51 +0530

    change

---
+++ HTML Programs/Login Page/Login 2/script1.js
@@ -0,0 +1,9 @@
+function toggleLogin() {
+    const loginBox = document.getElementById("loginBox");
+    if (loginBox.style.display === "block") {
+      loginBox.style.display = "none";
+    } else {
+      loginBox.style.display = "block";
+    }
+  }
+  
